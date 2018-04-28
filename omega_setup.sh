983d692230d197b9db4a7969a1aa4233c8f0bd9e
bob-design <38811403+bob-design@users.noreply.github.com>
2018-04-28 15:29:13 +0200

    Update omega_setup.sh

--- omega_setup.sh
+++ omega_setup.sh
@@ -55,7 +55,7 @@ if [ $DOSETUP =~ "y" ] ; then
   source ~/.bashrc
 fi
 
-wget https://github.com/omegacoinnetwork/omegacoin/archive/0.12.5.1.tar.gz
+wget wget https://github.com/omegacoinnetwork/omegacoin/releases/download/0.12.5/omegacoincore-0.12.5.1-linux64.tar.gz
 tar -xzf omegacoincore*.tar.gz
 sudo mv  omegacoincore*/bin/* /usr/bin
 
