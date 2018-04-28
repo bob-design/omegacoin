7f878c112800476e7c934e13e2a2cafab0f48624
bob-design <38811403+bob-design@users.noreply.github.com>
2018-04-28 15:54:43 +0200

    Update omega_setup.sh

--- omega_setup.sh
+++ omega_setup.sh
@@ -1,29 +1,46 @@
 #/bin/bash
 
 cd ~
-   apt-get update
-   apt-get -y upgrade
-   apt-get -y dist-upgrade
-   apt-get install -y nano htop git
-   apt-get install -y software-properties-common
-   apt-get install -y build-essential libtool autotools-dev pkg-config libssl-dev
-   apt-get install -y libboost-all-dev
-   apt-get install -y libevent-dev
-   apt-get install -y libminiupnpc-dev
-   apt-get install -y autoconf
-   apt-get install -y automake unzip
-   add-apt-repository  -y  ppa:bitcoin/bitcoin
-   apt-get update
-   apt-get install -y libdb4.8-dev libdb4.8++-dev
+echo "****************************************************************************"
+echo "* Ubuntu 16.04 is the recommended opearting system for this install.       *"
+echo "*                                                                          *"
+echo "* This script will install and configure your Omega Coin masternodes.      *"
+echo "****************************************************************************"
+echo && echo && echo
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "!                                                 !"
+echo "! Make sure you double check before hitting enter !"
+echo "!                                                 !"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo && echo && echo
+
+echo "Do you want to install all needed dependencies (no if you did it before)? y/n"
+read DOSETUP
+
+if [ $DOSETUP =~ "y" ] ; then
+  sudo apt-get update
+  sudo apt-get -y upgrade
+  sudo apt-get -y dist-upgrade
+  sudo apt-get install -y nano htop git
+  sudo apt-get install -y software-properties-common
+  sudo apt-get install -y build-essential libtool autotools-dev pkg-config libssl-dev
+  sudo apt-get install -y libboost-all-dev
+  sudo apt-get install -y libevent-dev
+  sudo apt-get install -y libminiupnpc-dev
+  sudo apt-get install -y autoconf
+  sudo apt-get install -y automake unzip
+  sudo add-apt-repository  -y  ppa:bitcoin/bitcoin
+  sudo apt-get update
+  sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
 
   cd /var
-   touch swap.img
-   chmod 600 swap.img
-   dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
-   mkswap /var/swap.img
-   swapon /var/swap.img
-   free
-   echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+  sudo touch swap.img
+  sudo chmod 600 swap.img
+  sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
+  sudo mkswap /var/swap.img
+  sudo swapon /var/swap.img
+  sudo free
+  sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
   cd
 
   sudo apt-get install -y ufw
@@ -36,11 +53,11 @@ cd ~
   mkdir -p ~/bin
   echo 'export PATH=~/bin:$PATH' > ~/.bash_aliases
   source ~/.bashrc
+fi
 
-wget https://github.com/omegacoinnetwork/omegacoin/releases/download/0.12.5.1/omagecoincore-0.12.5.1-linux64.zip
-unzip omagecoincore-0.12.5.1-linux64.zip -d .
-chmod +x *omega*
-rm -f omagecoincore-0.12.5.1-linux64.zip
+wget https://github.com/omegacoinnetwork/omegacoin/releases/download/0.12.5/omegacoincore-0.12.5-linux64.tar.gz
+tar -xzf omegacoincore*.tar.gz
+sudo mv  omegacoincore*/bin/* /usr/bin
 
 echo ""
 echo "Configure your masternodes now!"
@@ -67,13 +84,9 @@ echo "maxconnections=256" >> $CONF_DIR/$CONF_FILE
 echo "masternode=1" >> $CONF_DIR/$CONF_FILE
 echo "" >> $CONF_DIR/$CONF_FILE
 
-echo "addnode=80.211.155.141" >> $CONF_DIR/$CONF_FILE
-echo "addnode=159.65.128.239" >> $CONF_DIR/$CONF_FILE
-echo "addnode=45.76.134.148" >> $CONF_DIR/$CONF_FILE
-echo "addnode=140.82.7.244" >> $CONF_DIR/$CONF_FILE
-echo "addnode=185.58.224.83" >> $CONF_DIR/$CONF_FILE
-echo "addnode=199.247.29.167" >> $CONF_DIR/$CONF_FILE
-echo "addnode=107.191.48.250" >> $CONF_DIR/$CONF_FILE
+echo "addnode=142.208.127.121" >> $CONF_DIR/$CONF_FILE
+echo "addnode=154.208.127.121" >> $CONF_DIR/$CONF_FILE
+echo "addnode=142.208.122.127" >> $CONF_DIR/$CONF_FILE
 
 echo "" >> $CONF_DIR/$CONF_FILE
 echo "port=$PORT" >> $CONF_DIR/$CONF_FILE
@@ -110,7 +123,3 @@ rm -rf w*
 omegacoind -daemon
 
 watch omegacoin-cli getinfo
-
-
-
-
